ee54ae92b2141852e43faa670226c20028fc6125
oguilima <guilherme.lima@grupomfx.com>
2023-03-17 00:25:52 -0300

    sequelize

---
+++ 9_SEQUELIZE/1_instalacao/db/conn.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodesequelize', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+try{
+    sequelize.authenticate()
+
+    console.log('conectamos com sucesso com o sequelize')
+    
+}catch(err){
+    console.log('nao foi possível se conectar ao banco de dados ' + err )
+}   
+
+module.exports = sequelize
