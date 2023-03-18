8796bf4d79642dc89f9a45a3c5eb97b84ba77e8c
oguilima <guilherme.lima@grupomfx.com>
2023-03-18 02:33:31 -0300

    MVC

---
+++ 10_MVC/1_estrutura/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodemvc', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+try{
+    sequelize.authenticate()
+    console.log('conectamos ao mysql')
+}catch(err){
+    console.log('Não foi possível conectar: ' + err)
+}
+
+exports.default = sequelize
