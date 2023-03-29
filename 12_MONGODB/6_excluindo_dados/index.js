5a238a12d0bf3629c1eaebda40caad20b1e9238a
oguilima <oficialguilima@gmail.com>
2023-03-28 22:17:21 -0300

    resgatando dados

---
+++ 12_MONGODB/6_excluindo_dados/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+
+const productsRoutes = require('./routes/productsRoutes')
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.use('/favicon.ico', (req, res, next) => {
+  res.status(204).end()
+})
+
+
+app.use('/', productsRoutes)
+
+app.listen(3000)
